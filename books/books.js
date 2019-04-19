7abd435266c5a6c2eb58e0e73d440cc46e4735ce
Christopher Trauco <chris@trau.co>
2019-04-19 18:41:15 -0400

    TDD Kb-SRC-002-v7

--- books/books.js
+++ books/books.js
@@ -1,59 +1,45 @@
 // =================================================
-// Config
+// books.js Config 
 const express = require('express');
 const app = express();
-// Kb SRC 001 v4 @ 07:32; approx
+// Kb SRC 002 v4 @ 07:32; approx
 const bodyParser = require('body-parser');
-// Kb SRC 001 v4 @ 08:30; approx
+// Kb SRC 002 v4 @ 08:30; approx
 app.use(bodyParser.json());
 //load mongoose
 const mongoose = require('mongoose');
 // =================================================
-
-// =================================================
-// Connect Config
+// PORT CONFIG
 const PORT = 4545;
 // =================================================
-
-// =================================================
-// Route Config | Kb SRC 001 v6 @ 02:13; approx | Pro Tip: C
+// ROUTE CONFIG TO Db via Mongoose | Kb SRC 002 v6 @ 02:13; approx
 require("./Book")
-const Book = mongoose.model('Book')
+    const Book = mongoose.model('Book')
 // =================================================
-
-// =================================================
-// Kb SRC 001 v3 @ 05:52, approx
-// http://bit.ly/2Gvpq5d Resolves error (node:33884) DeprecationWarning: current URL string parser is deprecated, and will be removed in a future version. To use the new parser, pass option { useNewUrlParser: true } to MongoClient.connect.
+// Kb SRC 002 v3 @ 05:52, approx
+// http://bit.ly/2Gvpq5d | Resolves error (node:33884) DeprecationWarning: current URL string parser is deprecated, and will be removed in a future version. To use the new parser, pass option { useNewUrlParser: true } to MongoClient.connect.
 mongoose.connect('mongodb+srv://Trauco-Admin:SUevc5niqdWlehLe@cluster0-ab5qk.mongodb.net/test?retryWrites=true', () => {
     console.log('Now connected to MongoDB!')
 });
 // =================================================
-
-// =================================================
+// MAIN ENDPOINT
 app.get('/', (req, res) => {
-    res.send("This is the book services main endpoint!")
+    res.send("Main endpoint of books services.")
 })
 // =================================================
-
-// =================================================
-// Save JSON Response to DB | Kb SRC 001 v4 @ 01:50; approx 
+// Save JSON to DB | Kb SRC 002 v4 @ 01:50; approx 
 app.post('/book', (req, res) => {
-    // var newBook = {
-    const newBook = {
-    // let newBook = {
+    
+     const newBook = {
         title: req.body.title,
         author: req.body.author,
         numberPages: req.body.numberPages,
         publisher: req.body.publisher
-    }
-    // Kb SRC 001 v6 @ 07:00 - 07:20; approx
-    // Create a new Book with the above attributes
-    var book = new Book(newBook)
-    // TDD const and let
-    // const book = new Book(newBook)
-    // let book = new Book(newBook)
-
-    // Kb SRC 001 v6 @ 07:50; approx
+    }  
+// =================================================
+// ADD NEW BOOK TO DB | Kb SRC 002 v6 @ 07:00 - 07:20; approx
+const book = new Book(newBook)
+    // Kb SRC 002 v6 @ 07:50; approx
     book.save().then(() => {
         console.log('New Book created!')
     }).catch((err) => {
@@ -62,17 +48,59 @@ app.post('/book', (req, res) => {
         }
     })
     res.send('Booked added to MongoDB')
-
-        // =========================================
-        // console.log(req.body);
-        // // Kb SRC 001 v5 @ 00:52; approx
-        // res.send("00:D");
-        // =========================================
+})
+    // TDD
+    // =========================================
+    // console.log(req.body);
+    // Kb SRC 002 v5 @ 00:52; approx
+    // res.send("00:D");
+    // =========================================
+    // TDD END
+// =================================================
+// GET DB BOOKS ROUTE | Kb SRC 002 v7 @ 00:45; approx
+app.get('/books', (req, res) => {
+    //TDD
+    // =====================================
+    // console.log(req.body);
+    // console.log(res.send("00:D"));
+    Book.find().then((books) => {
+    // console.log(books) 
+        res.json(books)
+    }).catch(err => {
+        if(err){
+            throw err;
+        }
+    })
 });
 // =================================================
-
+// GET ONE BOOK BY ID | Kb SRC 002 v7 @ 04:27; approx
+app.get('/book/:id', (req, res) => {
+    // TDD =========================================
+    Book.findById(req.params.id).then((book) => {
+    // res.send(req.params.id)
+    // ============================================
+    // DOES BOOK DATA EXIST? | Kb SRC 002 v7 @ 10:37; approx
+    if(book){
+        // BOOK DATA
+        res.json(book)
+    }else{
+        res.sendStatus(404);
+    }
+    // ============================================
+    }).catch(err => {
+        if(err) {
+            throw err;
+        }
+    })
+})
+// =================================================
+// DELETE BOOK IN DB | Kb SRC 002 v8 @ 00:38; approx
+app.delete('/book/:id', (req, res) => {
+   // Kb SRC 002 v8 @ 02:00; approx
+   
+})
 // =================================================
 app.listen(PORT, () => {
     console.log(`Books service now listening on port ${PORT}.`);
-})
+})      // End
 // =================================================
